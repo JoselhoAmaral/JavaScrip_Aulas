d69d34ce014f4e15969db575e7958dd870b44b47
JoselhoAmaral <159856773+JoselhoAmaral@users.noreply.github.com>
2024-09-17 21:42:42 -0300

    Update aula 17-09-2024

--- Seção_08/Aula_108/script.js
+++ Seção_08/Aula_108/script.js
@@ -2,28 +2,33 @@
 
 function espera(msg, tempo) {
     return new Promise((resolve, reject) => {
+        if (typeof (msg) !== 'string') reject('BAD VALUE'); // verifica se eh string. Entao rejeita o valor e lanca no reject().
+
         setTimeout(() => {
-            resolve(msg);
+            resolve(msg); // este eh o retorno da Promise.
         }, tempo);
     });
 }
 
+// funcao que gera um tempo aleatorio
 function rand (min, max){
     min *= 1000;
     max *= 1000;
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-espera('Frase 1', rand(1, 3))
-    .then(resposta => {
+espera('Frase 1', rand(1, 3)) // primeira promise
+    .then(resposta => { // quando a promise dê certo, a resposta vai para o .then()
         console.log(resposta);
         return espera('Frase 2', rand(1, 3));
     })
-    .then(resposta => {
+    .then(resposta => { // segunda promise
         console.log(resposta);
-        return espera('Frase 3', rand(1, 3));
+        return espera(3543454, rand(1, 3));
     })
-    .then(resposta => {
+    .then(resposta => { // terceira promise
         console.log(resposta);
     })
-    .catch();
+    .catch(e => { // tudo que for cair no reject() vai para o .catch().
+        console.log('Erro: ', e);
+    });
