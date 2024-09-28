93d306e848b0a75ef57c5d24a27c85c632efa50c
JoselhoAmaral <159856773+JoselhoAmaral@users.noreply.github.com>
2024-09-27 22:28:52 -0300

    Update aulas 27-09-2024

---
+++ Secao_10/Aula_126/script.js
@@ -0,0 +1,17 @@
+// NPM - Node Package Manager
+/*
+No terminal:
+cd --> verifica em qual pasta está no momento
+cd Aula_126 --> Entra na pasta Aula_126
+npm init -y --> Para iniciar o npm
+npm install express --> instala o express
+"express": "^4.21.0" Esse express atualiza ate a versao 4.9x.9X, pois tem o simbolo ^. 
+npm install express -E --> Esse comando nunca permite que o express atualize.
+npm update --> Atualiza os pacotes
+npm install express --save-dev --> Instala o express no devDependencies 
+npm install express --save-prod --> Instala o express no dependencies
+npm install express@2.1.0 --> Instala o express na versao 2.1.0
+npm uninstall express --> desinstala o express
+npm ls --depth=1 --> mostra os pacotes instalados mais uma camada de profundidade por causa do "1". Se fosse "0" veria somente os pacotes instalados.
+npm outdated --> mostra os pacotes desatualizados.
+*/
